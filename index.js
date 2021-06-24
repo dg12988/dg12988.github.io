e7da5c0c73b4c68d31d99a0af5e7fa751d2313c0
dg12988 <doug.goldberg@me.com>
2021-06-24 16:09:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function flipImage(){
-    console.log(event.target);
+    console.log(Event.target);
  
     document.getElementById("DougThumbnail").style="transition: all 1s ease; transform: scaleX(-1);-webkit-transform: scaleX(-1);"
 
