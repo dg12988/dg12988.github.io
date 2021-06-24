28a39a3743133d09dad9c6a76f526e8d20881175
dg12988 <doug.goldberg@me.com>
2021-06-24 16:35:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function flipImage() {
-  if (event.target.id === "resumeButton")
+
+  if (event.target.id === "resumeButton"){
+    document.getElementById("resumeButton").style = "transition: all 1s ease; background-color: white;";
     document.getElementById("DougThumbnail").style =
-      "transition: all 1s ease; transform: scaleX(-1);-webkit-transform: scaleX(-1);";
+      "transition: all 1s ease; transform: scaleX(-1);-webkit-transform: scaleX(-1);";}
   else {
     document.getElementById("DougThumbnail").style =
       "transition: all 1s ease; transform: scaleX(1);-webkit-transform: scaleX(1);";
