0ac23da7e0478c6c76fb72de3c072cd2cbcd22de
dg12988 <doug.goldberg@me.com>
2021-06-24 16:39:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ function flipImage() {
 
   if (event.target.id === "resumeButton"){
     document.getElementById("resumeButton").style = "transition: all 1s ease; background-color: lightblue;";
+    document.getElementById("contactButton").style = "transition: all 1s ease; background-color: #723d46;";
     document.getElementById("DougThumbnail").style =
       "transition: all 1s ease; transform: scaleX(-1);-webkit-transform: scaleX(-1);";}
   else {
     document.getElementById("contactButton").style = "transition: all 1s ease; background-color: lightblue;";
+    document.getElementById("resumeButton").style = "transition: all 1s ease; background-color: #723d46;";
     document.getElementById("DougThumbnail").style =
       "transition: all 1s ease; transform: scaleX(1);-webkit-transform: scaleX(1);";
   }
