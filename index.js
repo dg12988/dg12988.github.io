a598ec96a1121363c8a0621fe246f3f07caafdbb
dg12988 <doug.goldberg@me.com>
2021-07-03 08:36:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -30,9 +30,10 @@ function openDialog(){
 
 function clearDialog(){
   var element = document.getElementById('dialogWrapper');
-
-  element.style.zIndex = -1;
+  
   element.style.opacity = 0;
-
+  setTimeout(() => {
+    element.style.zIndex = -1;
+  }, 500);
 }
 
