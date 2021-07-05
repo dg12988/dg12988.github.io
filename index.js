9cb40630d4d95502040ebb088457186d479add2e
dg12988 <doug.goldberg@me.com>
2021-07-05 07:51:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function startSlider(){
   var card2 = document.getElementById('card2');
   var card3 = document.getElementById('card3');
   setInterval(() => {
-   card2 = card3;
+   card2.innerHTML = card3.innerHTML;
   }, 3000);
 
 
