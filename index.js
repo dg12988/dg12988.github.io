a55af1bc1c8a314e09261c8a473726e9e7a3fa44
dg12988 <doug.goldberg@me.com>
2021-07-05 16:10:24 -0400

    Index Update

--- index.js
+++ index.js
@@ -1,47 +1,45 @@
 window.addEventListener('load', function () {
   document.getElementById("mainBanner").style = "width: 750px;height: 75px;"
 
-
-
   // Start Slider
   var card1 = document.getElementById('card1');
   var card2 = document.getElementById('card2');
   var card3 = document.getElementById('card3');
   var card4 = document.getElementById('card4');
+  var card5 = document.getElementById('card5');
+  var card6 = document.getElementById('card6');
 
   var tempC1 = card1.innerHTML;
   var tempC2 = card2.innerHTML;
   var tempC3 = card3.innerHTML;
   var tempC4 = card4.innerHTML;
+  var tempC5 = card4.innerHTML;
+  var tempC6 = card4.innerHTML;
 
   var n = 0;
-
-
 
 setInterval(() => {
   card1.style.opacity = .3;
   card2.style.opacity = .3;
   card3.style.opacity = .3;
 
- 
-
     setTimeout(() => {
       switch (n) {
         case 1:
-       card1.innerHTML = tempC1;
-         card2.innerHTML = tempC2;
-           card3.innerHTML = tempC3;
-          break;
+        card1.innerHTML = tempC1;
+        card2.innerHTML = tempC2;
+        card3.innerHTML = tempC3;
+        break;
         case 2:
-          card1.innerHTML = tempC4;
-          card2.innerHTML = tempC3;
-          card3.innerHTML = tempC1;
+         card1.innerHTML = tempC4;
+         card2.innerHTML = tempC5;
+         card3.innerHTML = tempC6;
           break;
         default:
-          n = 0;
-          card1.innerHTML = tempC1;
-          card2.innerHTML = tempC2;
-          card3.innerHTML = tempC3;
+         n = 0;
+         card1.innerHTML = tempC1;
+         card2.innerHTML = tempC2;
+         card3.innerHTML = tempC3;
       }
 
       card1.style.opacity = .9;
